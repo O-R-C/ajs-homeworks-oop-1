2443a580f9d9a0f6c48060b94395ef101d67409c
Andrey Semenov <semenov.orc@gmail.com>
2024-01-17 16:14:49 +0300

    add Zombie class & test

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,10 @@
 import Character from "./Classes/Character";
 import Bowman from "./Classes/Bowman";
 import Undead from "./Classes/Undead";
+import Swordsman from "./Classes/Swordsman";
+import Zombie from "./Classes/Zombie";
 
-const classes = [Character, Bowman, Undead];
+const classes = [Character, Bowman, Undead, Swordsman, Zombie];
 
 classes.forEach((characterClass) =>
   console.log(new characterClass(`${characterClass.name}`))

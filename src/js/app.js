35c67fb8fc02c8f99e816e7a38bfa244a5dd8dc4
Andrey Semenov <semenov.orc@gmail.com>
2024-01-17 16:17:03 +0300

    add Magician class & test

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,9 @@ import Bowman from "./Classes/Bowman";
 import Undead from "./Classes/Undead";
 import Swordsman from "./Classes/Swordsman";
 import Zombie from "./Classes/Zombie";
+import Magician from "./Classes/Magician";
 
-const classes = [Character, Bowman, Undead, Swordsman, Zombie];
+const classes = [Character, Bowman, Undead, Swordsman, Zombie, Magician];
 
 classes.forEach((characterClass) =>
   console.log(new characterClass(`${characterClass.name}`))

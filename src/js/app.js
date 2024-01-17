f80fdeb083536c5616e89d02ae5bd4e74aa0af29
Andrey Semenov <semenov.orc@gmail.com>
2024-01-17 16:21:12 +0300

    add Daemon class & test

--- src/js/app.js
+++ src/js/app.js
@@ -4,8 +4,17 @@ import Undead from "./Classes/Undead";
 import Swordsman from "./Classes/Swordsman";
 import Zombie from "./Classes/Zombie";
 import Magician from "./Classes/Magician";
+import Daemon from "./Classes/Daemon";
 
-const classes = [Character, Bowman, Undead, Swordsman, Zombie, Magician];
+const classes = [
+  Character,
+  Bowman,
+  Undead,
+  Swordsman,
+  Zombie,
+  Magician,
+  Daemon,
+];
 
 classes.forEach((characterClass) =>
   console.log(new characterClass(`${characterClass.name}`))

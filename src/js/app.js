94adfadc8951b846b741dde25af84dd18c5b3216
Andrey Semenov <semenov.orc@gmail.com>
2024-01-17 16:10:17 +0300

    add Swordsman class & test

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
 import Character from "./Classes/Character";
 import Bowman from "./Classes/Bowman";
+import Undead from "./Classes/Undead";
 
-const classes = [Character, Bowman];
+const classes = [Character, Bowman, Undead];
 
 classes.forEach((characterClass) =>
   console.log(new characterClass(`${characterClass.name}`))

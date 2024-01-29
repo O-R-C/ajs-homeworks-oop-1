692b6d62286d8a58824c27eaa240754ad9959fd0
Andrey Semenov <semenov.orc@gmail.com>
2024-01-29 12:28:22 +0300

    change allowedTypes.test.js

--- src/js/__tests__/allowedTypes.test.js
+++ src/js/__tests__/allowedTypes.test.js
@@ -8,20 +8,28 @@ import Zombie from "../Classes/Zombie";
 const team = [Bowman, Swordsman, Magician, Daemon, Undead, Zombie];
 
 describe("check characters", () => {
-  describe("to type", () => {
-    const allowedTypes = [
-      "Bowman",
-      "Swordsman",
-      "Magician",
-      "Daemon",
-      "Undead",
-      "Zombie",
-    ];
+  const allowedTypes = [
+    "Bowman",
+    "Swordsman",
+    "Magician",
+    "Daemon",
+    "Undead",
+    "Zombie",
+  ];
 
+  describe("to type", () => {
     test.each(allowedTypes)("%p", (type) => {
       const character = team.find((character) => character.name === type);
 
       expect(new character("test").type).toBe(type);
+    });
+  });
+
+  describe("to allowedType", () => {
+    test.each(allowedTypes)("%p", (type) => {
+      const character = team.find((character) => character.name === type);
+
+      expect(allowedTypes.includes(new character("test").type)).toBeTruthy();
     });
   });
 
